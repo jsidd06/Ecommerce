31b81bc720c8e0745cd90b06b45c7f4b6d9b33d6
SIDDHARTH JAIN <jsidd06@gmail.com>
2021-06-05 15:35:51 +0530

    yeh

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,18 @@ import data from "./data.js";
 
 const app = express();
 
+app.get('/api/products/:id', (req,res) => {
+    const product = data.products.find((x) => x.id === req.params.id) 
+    if(product){
+        res.send(product)
+    } else {
+        res.status(404).send({message: "Product not found"})
+    }
+})
 app.get("/api/products", (req,res) => {
     res.status(200).send(data.products)
 })
+
 
 app.get('/', (req,res) => {
     res.send("server is started")
