7f620a54b99d6b5e1bdd848a7624329b19358aef
SIDDHARTH JAIN <jsidd06@gmail.com>
2021-06-08 13:31:03 +0530

    yes

--- Backend/data.js
+++ Backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs"
+
 const data = {
+  users: [
+    {
+      name: 'sidd',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'john',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    }
+  ],
   products: [
     {
       id: "1",
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,16 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from "express"
 import data from "./data.js";
-
+import mongoose from "mongoose"
+import userRouter from "./Router/userRouter.js";
 const app = express();
+console.log(process.env.DB_STRING)
+mongoose.connect(process.env.DB_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
 
 app.get('/api/products/:id', (req,res) => {
     const product = data.products.find((x) => x.id === req.params.id) 
@@ -15,10 +24,15 @@ app.get("/api/products", (req,res) => {
     res.status(200).send(data.products)
 })
 
-
+app.use('/api/users', userRouter)
 app.get('/', (req,res) => {
     res.send("server is started")
 })
+
+app.use((err, req, res, next) =>{
+    res.status(500).send({message: err.message})
+})
+
 const port = process.env.PORT || 5000;
 app.listen(5000, () =>{
     console.log(`server at http://localhost${port}`);
