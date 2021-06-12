19fc8944a7fe59b5ecbb53a0bac554b5489ea014
SIDDHARTH JAIN <jsidd06@gmail.com>
2021-06-12 10:57:27 +0530

    place-order-screen

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -5,6 +5,7 @@ import { signout } from "./components/ActionProduct/userAction";
 import CartScreen from "./Screens/CartScreen";
 import HomeScreen from "./Screens/HomeScreen";
 import PaymentMethodScreen from "./Screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./Screens/PlaceOrderScreen";
 import ProductScreen from "./Screens/ProductScreen";
 import RegisterScreen from "./Screens/RegisterScreen";
 import ShippingAddressScreen from "./Screens/ShippingAddressScreen";
@@ -53,6 +54,7 @@ function App(props) {
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/shipping" component={ShippingAddressScreen}></Route>
           <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
